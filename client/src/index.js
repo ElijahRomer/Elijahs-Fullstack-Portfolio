be18dd8c1b3eb330a4e27daaa5adc2151a9c3338
Elijah Romer <romere88@gmail.com>
2022-02-15 17:57:32 -0600

    looks like the issue lies with the bootstrap import in the main index.js file in the client. Trying to adjust this path to troubleshoot heroku deployment.

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 // bring in Bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <BrowserRouter>
