53c438f63eba1a6248acb3ff2d27d74647290d35
Elijah Romer <romere88@gmail.com>
2022-02-15 18:11:44 -0600

    empty commit to re-run build

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+
 // bring in Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 
